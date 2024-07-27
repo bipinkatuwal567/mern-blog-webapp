201b42ecab6e792db7ff75b35cae159a3f82b000
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-07-27 19:38:18 +0545

    Create header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from "./pages/About";
 import Project from "./pages/Project";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,59 @@
+import { Button, Navbar, TextInput } from "flowbite-react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import Logo from "../assets/forest.png";
+import { AiOutlineMoon, AiOutlineSearch } from "react-icons/ai";
+
+const Header = () => {
+  const path = useLocation().pathname;
+  return (
+    <Navbar>
+      <Link to={"/"}>
+        <img src={Logo} className="flex-none w-10" />
+      </Link>
+
+      <form>
+        <TextInput
+          placeholder="Search..."
+          type="text"
+          className="hidden lg:inline"
+        />
+      </form>
+      <Button
+        className="h-10 w-12 flex justify-center items-center lg:hidden"
+        pill
+        color={"gray"}
+      >
+        <AiOutlineSearch className="text-lg" />
+      </Button>
+
+      <div className="flex justify-center items-center gap-2 lg:order-2">
+        <Button
+          className="h-10 w-12 flex justify-center items-center lg:hidden"
+          pill
+          color={"gray"}
+        >
+          <AiOutlineMoon className="text-lg" />
+        </Button>
+        <Button gradientDuoTone={"cyanToBlue"}>
+          <Link to={"sign-in"}>Sign in</Link>
+        </Button>
+        <Navbar.Toggle />
+      </div>
+
+      <Navbar.Collapse>
+        <Navbar.Link as={"div"} active={path === "/"}>
+          <Link to={"/"}>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={path === "/projects"}>
+          <Link to={"/projects"}>Projects</Link>
+        </Navbar.Link>
+        <Navbar.Link as={"div"} active={path === "/about"}>
+          <Link to={"/about"}>About</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
