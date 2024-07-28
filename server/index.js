2adfca7be3c217ebd3681ee762fb9c0f9828d0ca
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-07-28 15:51:26 +0545

    connect to database(mongodb)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 import express from "express";
-const app = express()
+import mongoose from "mongoose";
+import dotevn from "dotenv";
+
+dotevn.config();
+
+const app = express();
+mongoose
+  .connect(
+    process.env.DATABASE_URL
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+  console.log("Server is running on port 3000");
+});
