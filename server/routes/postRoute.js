74675fa7a1e7d6c871283589a253866c226a5c73
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-08-17 13:38:20 +0545

    create get post API route

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifyToken from "../utils/verifyToken.js";
-import { createPost } from "../controller/postController.js";
+import { createPost, getPost } from "../controller/postController.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createPost);
+router.get("/getpost", getPost);
 
 export default router;
